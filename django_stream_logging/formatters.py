06da3c487662a26851d0638553e2a8352aa5130b
cristhianCEM <cristianbcer27@gmail.com>
2024-11-10 19:38:16 -0600

    feat: add JsonLevelBasedFormatter class

--- django_stream_logging/formatters.py
+++ django_stream_logging/formatters.py
@@ -1,5 +1,7 @@
 import typing
 from logging import Formatter, LogRecord
+from json import dumps as json_dumps
+
 
 DEFAULT_LOG_FMT = '%(levelname)s : %(message)s'
 
@@ -21,3 +23,23 @@ class LevelBasedFormatter(Formatter):
     def format(self, record: LogRecord) -> str:
         formatter = self.formatters.get(record.levelname, self.default_formatter)
         return formatter.format(record) + '\n'
+
+
+class JsonLevelBasedFormatter(LevelBasedFormatter):
+    """
+    Formateador que convierte los registros de log en objetos JSON.
+    Maneja errores de serialización y convierte el timestamp a un formato legible.
+    """
+
+    def format(self, record: LogRecord) -> str:
+        formatter = self.formatters.get(record.levelname, self.default_formatter)
+        try:
+            message = formatter.format(record)
+        except Exception as e:
+            message = f'Error al serializar el log'
+        json_raw = json_dumps({
+            'level': record.levelname.lower(),
+            'message': message,
+            'timestamp': record.created
+        })
+        return f"data: {json_raw}\n\n"
